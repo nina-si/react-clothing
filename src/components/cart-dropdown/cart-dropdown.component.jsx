1adc874ee2d654b60e29f3f7638e6c51b50c7b9a
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-05-28 21:34:47 +0200

    refactor: cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -17,9 +17,9 @@ const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {cartItems.length &&
-          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)}
-        {!cartItems.length}
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
       <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
