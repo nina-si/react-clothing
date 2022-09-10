a6c9863bb1f0521469ccfb6dfa96793a86cb52c2
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-09-11 00:33:39 +0200

    fix: types for store and middlewares

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { compose, createStore, applyMiddleware, Middleware } from 'redux';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
@@ -9,14 +9,24 @@ import { rootReducer } from './root-reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const sagaMiddleware = createSagaMiddleware();
 
 const middleWares = [
   process.env.NODE_ENV === 'development' && logger,
   sagaMiddleware,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
-const persistConfig = {
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart'],
